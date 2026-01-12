454a8db48ec2fcd0c4834b3d5b08fc2bd877a708
JosuLopz_Dev <josulopz1824@gmail.com>
2026-01-12 17:41:08 -0600

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,16 +1,13 @@
 const productos = [
-    { id: 1, nombre: "Lámina de Zinc Aluzinc", categoria: "techos", imagen: "img/zinc.jpg", precio: "L. 250.00" },
-    { id: 2, nombre: "Taladro DeWalt 20V", categoria: "maquinas", imagen: "img/taladro.jpg", precio: "L. 3,500.00" },
-    { id: 3, nombre: "Martillo", categoria: "herramientas", imagen: "img/martillo.jpg", precio: "L. 180.00" },
-    { id: 4, nombre: "Bolsa de Cemento", categoria: "construccion", imagen: "img/cemento.jpg", precio: "L. 210.00" },
-    { id: 5, nombre: "Varilla", categoria: "construccion", imagen: "img/varilla.jpg", precio: "L. 145.00" },
-    { id: 6, nombre: "Pala", categoria: "construccion", imagen: "img/pala.jpg", precio: "L. 320.00" },
-    { id: 7, nombre: "Cemento Argos Gris", precio: 210, imagen: "cemento.jpg" },
-    { id: 8, nombre: "Varilla de hierro 3/8", precio: 500, imagen: "varilla.jpg" },
-    { id: 9, nombre: "Lámina de Zinc 10 pies", precio: 160, imagen: "zinc.jpg" },
-    { id: 10, nombre: "Soldadora Profesional", precio: 10000, imagen: "soldadora.jpg" },
-    { id: 11, nombre: "Clavos de madera 2", precio: 25, imagen: "clavo.jpg" },
-    { id: 12, nombre: "Tornillos punta de broca", precio: 15, imagen: "tornillo.jpg" }
+    { id: 1, nombre: "Lámina de Zinc Aluzinc", categoria: "techos", imagen: "https://tse1.explicit.bing.net/th/id/OIP.Emi3xdXv_U3mBpjEikPFTgHaFj?rs=1&pid=ImgDetMain&o=7&rm=3", precio: "L. 250.00" },
+    { id: 2, nombre: "Taladro DeWalt 20V", categoria: "maquinas", imagen: "https://http2.mlstatic.com/D_NQ_NP_633420-MLA45467093493_042021-F.jpg", precio: "L. 3,500.00" },
+    { id: 3, nombre: "Martillo", categoria: "herramientas", imagen: "https://tse2.mm.bing.net/th/id/OIP.tBKGVCFNd9TRhrLaUO6cDQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3", precio: "L. 180.00" },
+    { id: 4, nombre: "Bolsa de Cemento Argos", categoria: "construccion", imagen: "https://tse4.mm.bing.net/th/id/OIP.GAVtxJs6XfT-iklFYxkFXwHaGf?rs=1&pid=ImgDetMain&o=7&rm=3", precio: "L. 210.00" },
+    { id: 5, nombre: "Varilla", categoria: "construccion", imagen: "https://tse4.mm.bing.net/th/id/OIP.wbkCgjxcCfmwGSOHjoT9hQHaFj?rs=1&pid=ImgDetMain&o=7&rm=3", precio: "L. 145.00" },
+    { id: 6, nombre: "Pala", categoria: "construccion", imagen: "https://http2.mlstatic.com/pala-modelo-espanol-puno-y-41-plg-truper-17165-D_NQ_NP_307321-MLM20773934617_062016-F.jpg", precio: "L. 320.00" },
+    { id: 7, nombre: "Cemento Gris", precio: 210, imagen: "https://tse4.mm.bing.net/th/id/OIP.9dcoKOr-5ri-hfuJyidQYwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" },
+    { id: 10, nombre: "Soldadora Profesional", precio: 10000, imagen: "https://m.media-amazon.com/images/I/81F0aUMa0SL._SL1500_.jpg" },
+    { id: 12, nombre: "Tornillos punta de broca", precio: 15, imagen: "https://tse1.mm.bing.net/th/id/OIP.vSiiyX7x9QUaj-6G6_5xxAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" }
 ];
 
 // --- FUNCIONES DEL CATÁLOGO ---
@@ -90,4 +87,5 @@ window.onload = () => mostrarProductos('todos');
 document.addEventListener('keypress', (e) => {
     if(e.key === 'Enter' && document.activeElement.id === 'user-input') enviarMensaje();
 });
+
 
